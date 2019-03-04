b6ad83108fdd04eb2767f2f01046b42fb2086631
Tsarev Vladimir <sau412@gmail.com>
2019-03-04 15:46:29 +0300

    Create settings.php

---
+++ settings.php
@@ -0,0 +1,38 @@
+<?php
+// Settings file
+
+// DB variables
+$db_host="";
+$db_login="";
+$db_password="";
+$db_base="";
+
+// Resource variables
+$currency_name="Banano";
+$currency_symbol="BAN";
+$site_name="$currency_name miner";
+
+// Payout options
+$min_payout=0.01;
+$coin_per_mhash=1;
+
+// Coinimp keys
+$coinimp_public_key="";
+$coinimp_private_key="";
+$coinimp_web_site_key="";
+
+// Reddit keys
+
+// HTTP auth
+$reddit_http_login="";
+$reddit_http_password="";
+
+// Reddit login and password
+$reddit_username="";
+$reddit_password="=fMe5";
+
+$reddit_secret="";
+
+$reddit_useragent="Linux:banano_sender_bot:v_0 (by /u/YOUR_BOT_REDDIT_USERNAME_HERE)";
+
+?>
