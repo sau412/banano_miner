d5b03bb39f542b6f951c3530194a86eec250d13a
Tsarev Vladimir <sau412@gmail.com>
2019-03-04 15:49:12 +0300

    MySQL database

---
+++ db_scheme.sql
@@ -0,0 +1,34 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+CREATE TABLE `payouts` (
+  `uid` int(11) NOT NULL,
+  `address` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `amount` double NOT NULL,
+  `status` varchar(100) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'pending',
+  `txid` varchar(100) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+CREATE TABLE `stats` (
+  `uid` int(11) NOT NULL,
+  `id` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `address` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `balance` double NOT NULL DEFAULT '0',
+  `hashes` int(11) NOT NULL DEFAULT '0',
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+
+ALTER TABLE `payouts`
+  ADD PRIMARY KEY (`uid`);
+
+ALTER TABLE `stats`
+  ADD PRIMARY KEY (`uid`),
+  ADD UNIQUE KEY `grc_address` (`address`);
+
+
+ALTER TABLE `payouts`
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `stats`
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT;
