bea018c73329eb800d1e2dcd05e31a1b2d17afa3
Tsarev Vladimir <sau412@gmail.com>
2019-03-11 17:44:08 +0300

    New text, ref program, images

--- index.php
+++ index.php
@@ -7,11 +7,21 @@ require_once("coinimp.php");
 
 db_connect();
 
+if(isset($_POST['address']) && isset($_POST['ref_uid'])) {
+        $address=stripslashes($_POST['address']);
+        $address_html=html_escape($address);
+        $ref_uid=stripslashes($_POST['ref_uid']);
+        $user_id=get_user_id_by_address($address,$ref_uid);
+        header("Location: ./?address=$address_html");
+        die();
+}
+
 if(isset($_POST['action']) && $_POST['action']=='withdraw') {
         $address=stripslashes($_POST['address']);
         $address_html=html_escape($address);
         $address_escaped=db_escape($address);
 
+        db_query("LOCK TABLES `stats` WRITE, `payouts` WRITE");
         $currency_balance=db_query_to_variable("SELECT `balance` FROM `stats` WHERE `address`='$address_escaped'");
 
         //echo "Withdraw $grc_balance to address $address\n";
@@ -22,6 +32,7 @@ if(isset($_POST['action']) && $_POST['action']=='withdraw') {
                 db_query("UPDATE `stats` SET `balance`=0 WHERE `address`='$address_escaped'");
                 db_query("INSERT INTO `payouts` (`address`,`amount`) VALUES ('$address_escaped','$currency_balance_escaped')");
         }
+        db_query("UNLOCK TABLES");
 
         header("Location: ./?address=$address_html");
         die();
@@ -35,7 +46,8 @@ if(isset($_GET['json'])) {
         $user_id=get_user_id_by_address($address);
 
         $hashes_balance=coinimp_get_user_balance("web",$user_id);
-        $hashes_escaped=db_escape($hashes_balance);
+        user_balance_add($address,$hashes_balance);
+        /*$hashes_escaped=db_escape($hashes_balance);
         //var_dump($hashes_balance);
 
         $prev_hashes=db_query_to_variable("SELECT `hashes` FROM `stats` WHERE `address`='$address_escaped'");
@@ -44,20 +56,13 @@ if(isset($_GET['json'])) {
                 $currency_balance_delta=$coin_per_mhash*$hashes_delta/1000000;
                 $currency_balance_delta_escaped=db_escape($currency_balance_delta);
                 db_query("UPDATE `stats` SET `hashes`='$hashes_escaped',`balance`=`balance`+'$currency_balance_delta_escaped' WHERE `address`='$address_escaped'");
-        }
+        }*/
         $currency_balance=db_query_to_variable("SELECT `balance` FROM `stats` WHERE `address`='$address_escaped'");
 
         $currency_balance=sprintf("%0.8f",$currency_balance);
 
         if($currency_balance>$min_payout) {
-                $withdraw_form=<<<_END
-<form name=withdraw method=POST style='display:inline;'>
-<input type=hidden name=action value='withdraw'>
-<input type=hidden name=address value='$address_html'>
-<input type=submit value='withdraw'>
-</form>
-
-_END;
+                $withdraw_form=html_withdraw_form($address);
         } else {
                 $withdraw_form="min payout is $min_payout $currency_symbol";
         }
@@ -66,7 +71,7 @@ _END;
 <tr><td align=right>Address:</td><td>$address_html</td></tr>
 <!--<tr><td align=right>Hashes mined:</td><td>$hashes_balance</td></tr>-->
 <!--<tr><td align=right>$currency_name per Mhash:</td><td>$coin_per_mhash</td></tr>-->
-<tr><td align=right>Confirmed balance:</td><td>$currency_balance $currency_symbol $withdraw_form</td></tr>
+<tr><td align=right>Current balance:</td><td>$currency_balance $currency_symbol $withdraw_form</td></tr>
 </table>
 <br>
 
@@ -77,8 +82,8 @@ _END;
 // Standard page beginning
 echo html_page_begin($site_name);
 echo <<<_END
-<h1>$site_name</h1>
-<p>Disclaimer: this page embeds third-party script (from coinimp), use it on your own risk.</p>
+<h1><img src='logo-horizontal.png' alt='$site_name' width=50%></h1>
+
 _END;
 
 if(isset($_GET['address'])) {
@@ -90,13 +95,38 @@ if(isset($_GET['address'])) {
         $user_id=get_user_id_by_address($address);
 
         echo html_coinimp_frame("web",$user_id);
-} else {
-        echo <<<_END
-<form name=address method=GET>
-<p>Enter address carefully, you cannot move mined funds to another address.</p>
-<p>Your $currency_name address: <input type=text size=80 name=address> <input type=submit value='Open miner page'></p>
 
+        echo html_ref_section($address);
+
+} else {
+        if(isset($_GET['r'])) {
+                $ref_uid=stripslashes($_GET['r']);
+                if(!is_numeric($ref_uid)) $ref_uid=0;
+        } else {
+                $ref_uid=0;
+        }
+        echo <<<_END
+<p><a href='https://banano.cc/'><img src='bananosite.jpg' height=120px></a> <a href='https://banano.how/'><img src='bananohow.jpg' height=120px></a></p>
+
+<p>Welcome to BANANO Miner!<br>
+This is a community-made faucet you can use to "mine" some fresh BANANO.<br>
+You might know that BANANO itself doesn't require mining, the faucet actually mines webchain by using your CPU,<br>
+automatically converts rewards to BANANO and sends it to your wallet.</p>
+
+<p>This wouldn't be very efficient normally, but this faucet is subsidized by the BANANO team,<br>
+meaning you will actually get much more rewards than the coins you mine.<br>
+The proof-of-work provided here is rather used to avoid exploits and to make BANANO distribution fair.</p>
+
+<p>Disclaimer: this page embeds third-party script (from coinimp), use it on your own risk.</p>
+
+<p>Enter address carefully, you cannot move mined funds to another address.</p>
+
+<form name=address method=POST>
+<input type=hidden name=ref_uid value='$ref_uid'>
+<p>Your $currency_name address: <input type=text size=80 name=address> <input type=submit value='Open miner page'></p>
 </form>
+
+<p>New to BANANO? Check the <a href='https://banano.cc/'>official website</a>! If you need help getting started visit <a href='https://banano.how/'>banano.how</a></p>
 
 _END;
 }
