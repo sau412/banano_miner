d8e6e327c003ff8664d37a4553c5fe3d4a732ec3
Tsarev Vladimir <sau412@gmail.com>
2019-03-11 17:45:29 +0300

    New settings here

--- settings.php
+++ settings.php
@@ -11,10 +11,14 @@ $db_base="";
 $currency_name="Banano";
 $currency_symbol="BAN";
 $site_name="$currency_name miner";
+$site_url="https://bananominer.arikado.ru/";
 
 // Payout options
 $min_payout=0.01;
 $coin_per_mhash=5.83; // actual rate for 4th of March 2019
+
+$ref_level_1=0.1;
+$ref_level_2=0.05;
 
 // Coinimp keys
 $coinimp_public_key="";
