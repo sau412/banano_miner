225385d2c048dadc8de53e19659c2d5040cb9944
Tsarev Vladimir <sau412@gmail.com>
2019-03-19 18:00:57 +0300

    ip_stats table

--- db_scheme.sql
+++ db_scheme.sql
@@ -1,6 +1,13 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+CREATE TABLE `ip_stats` (
+  `uid` int(11) NOT NULL,
+  `ip` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
+  `balance` double NOT NULL DEFAULT '0',
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
 CREATE TABLE `payouts` (
   `uid` int(11) NOT NULL,
   `address` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
@@ -16,9 +23,14 @@ CREATE TABLE `stats` (
   `address` varchar(100) COLLATE utf8_unicode_ci NOT NULL,
   `balance` double NOT NULL DEFAULT '0',
   `hashes` int(11) NOT NULL DEFAULT '0',
+  `ref_uid` int(11) NOT NULL DEFAULT '0',
   `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+
+ALTER TABLE `ip_stats`
+  ADD PRIMARY KEY (`uid`),
+  ADD UNIQUE KEY `ip` (`ip`);
 
 ALTER TABLE `payouts`
   ADD PRIMARY KEY (`uid`);
@@ -28,6 +40,8 @@ ALTER TABLE `stats`
   ADD UNIQUE KEY `grc_address` (`address`);
 
 
+ALTER TABLE `ip_stats`
+  MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT;
 ALTER TABLE `payouts`
   MODIFY `uid` int(11) NOT NULL AUTO_INCREMENT;
 ALTER TABLE `stats`
